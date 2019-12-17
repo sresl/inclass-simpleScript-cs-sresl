eb271147833411b19430ef793b870ab7e2458857
sresl <sarah.resl21@gmail.com>
2019-12-17 16:07:42 +0100

    changed variable names

--- Assets/MyGame/Script/ComputeNumbers.cs
+++ Assets/MyGame/Script/ComputeNumbers.cs
@@ -2,40 +2,46 @@
 using UnityEngine.UI;
 using TMPro;
 
-    
-
 
 public class ComputeNumbers : MonoBehaviour
 { 
     public TextMeshProUGUI result;
-    public TMP_InputField varA;
-    public TMP_InputField varB;
-    public Button btn_reset;
-    public Button btn_sub_Numbers;
+
+    public TMP_InputField leftNumber;
+    public TMP_InputField rightNumber;
+
+    public Button btnReset;
+    public Button btnResult;
+
 
 public void SetResult()
 {
     result.text = AddNumbers().ToString();
-        varA.interactable = false;
-        varB.interactable = false;
-            btn_sub_Numbers.interactable = false;
-                btn_reset.interactable = true;
+
+        leftNumber.interactable = false;
+        rightNumber.interactable = false;
+
+            btnResult.interactable = false;
+            btnReset.interactable = true;
 }
 
 public void Reset()
 {
-    varA.text = "0";
-    varB.text = "0";
-        varA.interactable = true;
-        varB.interactable = true;
-            btn_sub_Numbers.interactable = true;
-                btn_reset.interactable = false;
+    leftNumber.text = "";
+    rightNumber.text = "";
+
+        leftNumber.interactable = true;
+        rightNumber.interactable = true;
+
+           btnResult.interactable = true;
+           btnReset.interactable = false;
 
     result.text = "Result";
 }
+
 private float AddNumbers()
 {
-    float tempResult = float.Parse(varA.text) - float.Parse(varB.text);
+    float tempResult = float.Parse(leftNumber.text) - float.Parse(rightNumber.text);
     
     return tempResult;
 }
