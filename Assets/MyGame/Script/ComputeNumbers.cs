7646d1c42610ae8364ebfad901275d21676c39cb
sresl <sarah.resl21@gmail.com>
2019-12-17 16:35:38 +0100

    add comments into the code and set new identifiers to the right objects in unity

--- Assets/MyGame/Script/ComputeNumbers.cs
+++ Assets/MyGame/Script/ComputeNumbers.cs
@@ -17,26 +17,36 @@ public class ComputeNumbers : MonoBehaviour
 
 public void SetResult()
 {
+
+// Set Result
     result.text = SubNumbers().ToString();
 
+    
+//no interaction with inputfields
         leftNumber.interactable = false;
         rightNumber.interactable = false;
 
+//activate Reset Button
             btnResult.interactable = false;
             btnReset.interactable = true;
 }
 
 public void Reset()
 {
+
+//Reset both Inputfields back to "Add Number" (reset)
     leftNumber.text = "";
     rightNumber.text = "";
 
+//interact with inputfields
         leftNumber.interactable = true;
         rightNumber.interactable = true;
 
+//activate Buttons
            btnResult.interactable = true;
            btnReset.interactable = false;
 
+// Reset Result Text
     result.text = FinalResult;
 }
 
